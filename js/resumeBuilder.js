6aed48a3fdd77de052be76950ebde23f4a75bf74
Anatoliy Lushnikov <toliantm@narod.ru>
2015-05-29 10:45:17 +0200

    CR fixes
    Fixed filename
    Realigned JS
    Added missing semicolon

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,201 +1,201 @@
-var fillData = function(templ, data) {
-      return templ.replace("%data%", data);
+var fillData = function (templ, data) {
+    return templ.replace("%data%", data);
 };
 
 var bio = {
-      "name" : "Dog",
-      "role" : "The Developer",
-      "contacts" : {
-            "mobile": "iphone",
-            "email": "anatolylushnikov@gmail.com",
-            "github": "https://github.com/genesisuda",
-            "twitter": "no twitter",
-            "location": "Kaliningrad"
-      },
-      "welcomeMessage": "Hi there",
-      "skills": ["woof", "bark", "hardcore coding"],
-      "biopic": "images/dog.jpg",
-      display: function() {
-            $("#header").prepend(
-                  fillData(HTMLheaderName, this.name) +
-                  fillData(HTMLheaderRole, this.role));
-            $("#topContacts").append(fillData(HTMLmobile, this.contacts.mobile));
-            $("#topContacts").append(fillData(HTMLemail, this.contacts.email));
-            $("#topContacts").append(fillData(HTMLtwitter, this.contacts.twitter));
-            $("#topContacts").append(fillData(HTMLgithub, this.contacts.github));
-            $("#topContacts").append(fillData(HTMLblog, "not yet"));
-            $("#topContacts").append(fillData(HTMLlocation, this.contacts.location));
+    "name" : "Dog",
+    "role" : "The Developer",
+    "contacts" : {
+        "mobile": "iphone",
+        "email": "anatolylushnikov@gmail.com",
+        "github": "https://github.com/genesisuda",
+        "twitter": "no twitter",
+        "location": "Kaliningrad"
+    },
+    "welcomeMessage": "Hi there",
+    "skills": ["woof", "bark", "hardcore coding"],
+    "biopic": "images/dog.jpg",
+    display: function () {
+        $("#header").prepend(
+            fillData(HTMLheaderName, this.name) +
+            fillData(HTMLheaderRole, this.role));
+        $("#topContacts").append(fillData(HTMLmobile, this.contacts.mobile));
+        $("#topContacts").append(fillData(HTMLemail, this.contacts.email));
+        $("#topContacts").append(fillData(HTMLtwitter, this.contacts.twitter));
+        $("#topContacts").append(fillData(HTMLgithub, this.contacts.github));
+        $("#topContacts").append(fillData(HTMLblog, "not yet"));
+        $("#topContacts").append(fillData(HTMLlocation, this.contacts.location));
 
-            $("#footerContacts").append(fillData(HTMLmobile, this.contacts.mobile));
-            $("#footerContacts").append(fillData(HTMLemail, this.contacts.email));
-            $("#footerContacts").append(fillData(HTMLtwitter, this.contacts.twitter));
-            $("#footerContacts").append(fillData(HTMLgithub, this.contacts.github));
-            $("#footerContacts").append(fillData(HTMLblog, "not yet"));
-            $("#footerContacts").append(fillData(HTMLlocation, this.contacts.location));
+        $("#footerContacts").append(fillData(HTMLmobile, this.contacts.mobile));
+        $("#footerContacts").append(fillData(HTMLemail, this.contacts.email));
+        $("#footerContacts").append(fillData(HTMLtwitter, this.contacts.twitter));
+        $("#footerContacts").append(fillData(HTMLgithub, this.contacts.github));
+        $("#footerContacts").append(fillData(HTMLblog, "not yet"));
+        $("#footerContacts").append(fillData(HTMLlocation, this.contacts.location));
 
-            $("#header").append(fillData(HTMLbioPic, this.biopic));
-            $("#header").append(fillData(HTMLwelcomeMsg, this.welcomeMessage));
+        $("#header").append(fillData(HTMLbioPic, this.biopic));
+        $("#header").append(fillData(HTMLwelcomeMsg, this.welcomeMessage));
 
-            var elSkills = $(HTMLskillsStart);
+        var elSkills = $(HTMLskillsStart);
 
-            $("#header").append(elSkills[0]);
+        $("#header").append(elSkills[0]);
 
-            $.each(this.skills, function (i, v) {
-                  $(elSkills[1]).append(fillData(HTMLskills, v));
-            });
+        $.each(this.skills, function (i, v) {
+            $(elSkills[1]).append(fillData(HTMLskills, v));
+        });
 
-            $("#header").append(elSkills[1]);
-      }
+        $("#header").append(elSkills[1]);
+    }
 };
 
 var education = {
-      "schools": [{
-            "name": "Woof Federal University",
-            "location": "Kaliningrad",
-            "degree": "Specialist degree in Applied maths and informatics",
-            "majors": ["Numeric methods"],
-            "dates": 2013,
-            "url": "http://kantiana.ru"
-      },{
-            "name": "Woof Federal University",
-            "location": "Kaliningrad",
-            "degree": "Specialist degree in Applied maths and informatics",
-            "majors": ["Numeric methods"],
-            "dates": 2013,
-            "url": "http://kantiana.ru"
-      }],
-      "onlineCourses": [{
-            "title": "Front-End Web Developer Nanodegree",
-            "school": "Udacity",
-            "date": 2015,
-            "url": "https://www.udacity.com/course/nd001"
-      },{
-            "title": "Data Analyst Nanodegree",
-            "school": "Udacity",
-            "date": 2015,
-            "url": "https://www.udacity.com/course/nd002"
-      }],
-      display: function (){
-            $.each(this.schools, function (i, v) {
-                  var $eEntry = $(HTMLschoolStart);
+    "schools": [{
+        "name": "Woof Federal University",
+        "location": "Kaliningrad",
+        "degree": "Specialist degree in Applied maths and informatics",
+        "majors": ["Numeric methods"],
+        "dates": 2013,
+        "url": "http://kantiana.ru"
+    },{
+        "name": "Woof Federal University",
+        "location": "Kaliningrad",
+        "degree": "Specialist degree in Applied maths and informatics",
+        "majors": ["Numeric methods"],
+        "dates": 2013,
+        "url": "http://kantiana.ru"
+    }],
+    "onlineCourses": [{
+        "title": "Front-End Web Developer Nanodegree",
+        "school": "Udacity",
+        "date": 2015,
+        "url": "https://www.udacity.com/course/nd001"
+    },{
+        "title": "Data Analyst Nanodegree",
+        "school": "Udacity",
+        "date": 2015,
+        "url": "https://www.udacity.com/course/nd002"
+    }],
+    display: function (){
+        $.each(this.schools, function (i, v) {
+            var $eEntry = $(HTMLschoolStart);
 
-                  $eEntry.append(
-                        fillData(HTMLschoolName, v.name) +
-                        fillData(HTMLschoolDegree, v.degree));
-                  $eEntry.append(fillData(HTMLschoolDates, v.dates));
-                  $eEntry.append(fillData(HTMLschoolLocation, v.location));
-                  $eEntry.append(fillData(HTMLschoolMajor, v.majors));
+            $eEntry.append(
+                fillData(HTMLschoolName, v.name) +
+                fillData(HTMLschoolDegree, v.degree));
+            $eEntry.append(fillData(HTMLschoolDates, v.dates));
+            $eEntry.append(fillData(HTMLschoolLocation, v.location));
+            $eEntry.append(fillData(HTMLschoolMajor, v.majors));
 
-                  $("#education").append($eEntry);
-            });
+            $("#education").append($eEntry);
+        });
 
-            $("#education").append(HTMLonlineClasses);
+        $("#education").append(HTMLonlineClasses);
 
-            $.each(this.onlineCourses, function (i, v) {
-                  var $oeEntry = $(HTMLschoolStart);
+        $.each(this.onlineCourses, function (i, v) {
+            var $oeEntry = $(HTMLschoolStart);
 
-                  $oeEntry.append(
-                        fillData(HTMLonlineTitle, v.title) +
-                        fillData(HTMLonlineSchool, v.school));
-                  $oeEntry.append(fillData(HTMLonlineDates, v.date));
-                  $oeEntry.append(fillData(HTMLonlineURL, v.url));
+            $oeEntry.append(
+                fillData(HTMLonlineTitle, v.title) +
+                fillData(HTMLonlineSchool, v.school));
+            $oeEntry.append(fillData(HTMLonlineDates, v.date));
+            $oeEntry.append(fillData(HTMLonlineURL, v.url));
 
-                  $("#education").append($oeEntry);
-            });
-      }
+            $("#education").append($oeEntry);
+        });
+    }
 };
 
 var work = {
-      "jobs": [{
-            "employer": "DevFactory",
-            "title": "Technical Architect",
-            "location": "Kaliningrad",
-            "dates": "2012-2015",
-            "description": "woof woof",
-      }],
-      display: function () {
-            $.each(this.jobs, function (i, v) {
-                  var $wEntry = $(HTMLworkStart);
+    "jobs": [{
+        "employer": "DevFactory",
+        "title": "Technical Architect",
+        "location": "Kaliningrad",
+        "dates": "2012-2015",
+        "description": "woof woof",
+    }],
+    display: function () {
+        $.each(this.jobs, function (i, v) {
+            var $wEntry = $(HTMLworkStart);
 
-                  $wEntry.append(
-                        fillData(HTMLworkEmployer, v.employer) +
-                        fillData(HTMLworkTitle, v.title));
-                  $wEntry.append(fillData(HTMLworkDates, v.dates));
-                  $wEntry.append(fillData(HTMLworkLocation, v.location));
-                  $wEntry.append(fillData(HTMLworkDescription, v.description));
+            $wEntry.append(
+                fillData(HTMLworkEmployer, v.employer) +
+                fillData(HTMLworkTitle, v.title));
+            $wEntry.append(fillData(HTMLworkDates, v.dates));
+            $wEntry.append(fillData(HTMLworkLocation, v.location));
+            $wEntry.append(fillData(HTMLworkDescription, v.description));
 
-                  $("#workExperience").append($wEntry);
-            })
-      }
+            $("#workExperience").append($wEntry);
+        });
+    }
 };
 
 var projects = {
-      "projects": [{
-            "title": "GSoC 2012",
-            "dates": "2012/06/01-2012/09/01",
-            "description": "Work done during GSoC 2012",
-            "images": ["images/gsoc.png"],
-      }],
-      display: function () {
-            $.each(this.projects, function (i, v) {
-                  var $pEntry = $(HTMLprojectStart);
+    "projects": [{
+        "title": "GSoC 2012",
+        "dates": "2012/06/01-2012/09/01",
+        "description": "Work done during GSoC 2012",
+        "images": ["images/GSoC.png"],
+    }],
+    display: function () {
+        $.each(this.projects, function (i, v) {
+            var $pEntry = $(HTMLprojectStart);
 
-                  $pEntry.append(fillData(HTMLprojectTitle, v.title));
-                  $pEntry.append(fillData(HTMLprojectDates, v.dates));
-                  $pEntry.append(fillData(HTMLprojectDescription, v.description));
-                  $pEntry.append(fillData(HTMLprojectImage, v.images));
+            $pEntry.append(fillData(HTMLprojectTitle, v.title));
+            $pEntry.append(fillData(HTMLprojectDates, v.dates));
+            $pEntry.append(fillData(HTMLprojectDescription, v.description));
+            $pEntry.append(fillData(HTMLprojectImage, v.images));
 
-                  $("#projects").append($pEntry);
-            });
-      }
+            $("#projects").append($pEntry);
+        });
+    }
 };
 
 /* as we have some show/hide code in html can"t use document.ready
    so using this approach instead
-*/
-(function(){
-      bio.display();
-      education.display();
-      work.display();
-      projects.display();
+   */
+   (function(){
+    bio.display();
+    education.display();
+    work.display();
+    projects.display();
 
-      $("#mapDiv").append(googleMap);
+    $("#mapDiv").append(googleMap);
 })();
 
 $(document).ready(function() {
-      // so not to recalculate this on mousemove
-      var pic = $(".biopic")[0];
-      var rC = {
-            x : pic.getBoundingClientRect().left + pic.clientWidth / 2,
-            y : pic.getBoundingClientRect().top + pic.clientHeight / 2
-      };
+// so not to recalculate this on mousemove
+var pic = $(".biopic")[0];
+var rC = {
+    x : pic.getBoundingClientRect().left + pic.clientWidth / 2,
+    y : pic.getBoundingClientRect().top + pic.clientHeight / 2
+};
 
-      // applying some html fixes (we"re not supposed to modify html hardly for
-      // this task, right? )
-      $("#skillsH3").before("<div id=\"skillsContainer\"></div>");
-      $("#skillsH3").detach().appendTo("#skillsContainer");
-      $("#skills").detach().appendTo("#skillsContainer");
+// applying some html fixes (we"re not supposed to modify html hardly for
+// this task, right? )
+$("#skillsH3").before("<div id=\"skillsContainer\"></div>");
+$("#skillsH3").detach().appendTo("#skillsContainer");
+$("#skills").detach().appendTo("#skillsContainer");
 
-      $(".container").mousemove(function(evt) {
-            var mouseX = event.clientX;
-            var mouseY = event.clientY;
+$(".container").mousemove(function(evt) {
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
 
-            var rX = (mouseX - rC.x) / 20;
-            var rY = (mouseY - rC.y) / 20;
-            if (rX < -45) {
-                  rX = -45;
-            } else if (rX > 45) {
-                  rX = 45;
-            }
+    var rX = (mouseX - rC.x) / 20;
+    var rY = (mouseY - rC.y) / 20;
+    if (rX < -45) {
+        rX = -45;
+    } else if (rX > 45) {
+        rX = 45;
+    }
 
-            rX = rX < 0 ? 180 - rX : rX;
+    rX = rX < 0 ? 180 - rX : rX;
 
-            if (rY < -45) {
-                  rY = -45;
-            } else if (rY > 45) {
-                  rY = 45;
-            }
+    if (rY < -45) {
+        rY = -45;
+    } else if (rY > 45) {
+        rY = 45;
+    }
 
-            $(pic).css("transform", "rotateX(rYdeg) rotateY(-rXdeg)".replace("rX",rX).replace("rY",rY));
-      });
+    $(pic).css("transform", "rotateX(rYdeg) rotateY(-rXdeg)".replace("rX",rX).replace("rY",rY));
+});
 });
